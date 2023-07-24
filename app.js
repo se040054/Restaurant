34f1669fc8f1eb4a78fa6789206f34c90d8cf325
se040054 <peter8822967@gmail.com>
2023-07-24 23:03:52 +0800

    route :get create,post form  , form data will add in Restaurant table

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express()
 const port = 3000
 const db=require('./models')
 const Restaurant=db.Restaurant
+const bodyParser = require('body-parser')
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/',(req,res)=>{
   res.redirect('/restaurants')
 })
@@ -20,7 +21,25 @@ app.get('/restaurants',(req,res)=>{
   })
     .then((restaurants)=>res.render('index',{restaurants}))
 })
+app.get('/restaurants/create',(req,res)=>{
+  res.render('create')
+})
 
+app.post('/restaurants',(req,res)=>{
+  const newRestaurant= req.body
+  console.log(newRestaurant)
+  return Restaurant.create({
+      name: newRestaurant.name,
+      name_en: newRestaurant.name_en,
+      category: newRestaurant.category,
+      image: newRestaurant.image,
+      location: newRestaurant.location,
+      phone: newRestaurant.phone,
+      google_map: newRestaurant.google_map,
+      rating: newRestaurant.rating,
+      description: newRestaurant.description
+  }).then(()=>res.redirect('/restaurants'))
+})
 app.listen(port, () => {
   console.log(`Click : http://localhost:3000`)
 })
