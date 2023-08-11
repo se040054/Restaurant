a79a2e835f671e5958cbbd022a3f4ada423217b9
se040054 <peter8822967@gmail.com>
2023-08-11 11:30:28 +0800

    passport seiton  , css

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-'use strict'
-const express = require('express')
-const app = express()
-const { engine } = require('express-handlebars')
-const port = 3000
-const bodyParser = require('body-parser')
+"use strict";
+const express = require("express");
+const app = express();
+const { engine } = require("express-handlebars");
+const port = 3000;
+const bodyParser = require("body-parser");
 
-const methodOverride = require('method-override')
+const methodOverride = require("method-override");
 const flash = require("connect-flash");
 const session = require("express-session");
-const messageHandler = require('./middleware/message-handler')
-const errorHandler = require('./middleware/error-handler')
+const messageHandler = require("./middleware/message-handler");
+const errorHandler = require("./middleware/error-handler");
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
-const passport = require("passport"); //要放在變數後面
+const passport = require("./config/passport"); //要放在變數後面
 const router = require("./routers"); //要放在變數後面
-app.engine('.hbs', engine({ extname: '.hbs' }))
-app.set('view engine', '.hbs')
-app.set('views', './views')
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(flash());
-
 
 app.use(
   session({
@@ -35,11 +34,11 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(messageHandler)
+app.use(messageHandler);
 
-app.use(router)
-app.use(errorHandler)
+app.use(router);
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('Click : http://localhost:3000')
-})
+  console.log("Click : http://localhost:3000");
+});
--- routers/users.js
+++ routers/users.js
@@ -3,78 +3,7 @@ const router = express.Router();
 const db = require("../models");
 const User = db.User;
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const { where } = require("sequelize");
-const bcrypt = require("bcryptjs");
-const GoogleStrategy = require("passport-google-oauth20");
-const { raw } = require("mysql2");
-passport.use( 
-  new LocalStrategy({ usernameField: "email" }, (username, password, done) => {
-    return User.findOne({
-      attributes: ["id", "name", "email", "password"],
-      where: { email: username },
-      raw: true,
-    })
-      .then((user) => {
-        if (!user) {
-          return done(null, false, { message: "查無帳號" }); 
-        }//登入策略時 不要用redirect導向 而是用done ，也不要寫flash(抓不到)
-        return bcrypt.compare(password, user.password).then((isMatch) => {
-          if (!isMatch) {
-            return done(null,false,{message:"密碼錯誤"}) 
-          }
-          return done(null, user);
-        });
-      })
-      .catch((error) => {
-        error.errorMessage = "伺服器發生問題 請稍後再登入";
-        done(error);
-      });
-  })
-);
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      scope: ["profile" , "email"], //要請求的資料
-      state: true,
-    },
-     (accessToken, refreshToken, profile, done)=> {
-      const {displayName} = profile
-      const email = profile.emails[0].value
-      console.log(profile)
-      return User.findOne({
-        attributes:['id' , 'email' , 'name' ],
-        where : {email},
-        raw:true
-      }).then(async(user)=>{
-        if (!user){
-          const randomPwd = Math.random().toString(36).slice(-8);
-          const hash = await bcrypt.hash(randomPwd,10)
-          newUser = await User.create({ name:displayName, email, password:hash });
-          return  done(null,{id: newUser.id,name: newUser.name,email: newUser.email,})
-        }
-       return await done(null,user)
-      }).catch((error)=>{
-        error.errorMessage="使用GOOGLE登入失敗"
-        done(error)
-      })
-    }
-  )
-);
-  
-passport.serializeUser((user, done) => {
-  const { id, name, email } = user;
-  return done(null, { id, name, email });
-});
-
-passport.deserializeUser((user, done) => {
-  return done(null, { id: user.id ,name:user.name,email:user.email });
-});
-
+const bcrypt=require('bcryptjs')
 router.get("/login", (req, res) => {
   res.render("login");
 });
